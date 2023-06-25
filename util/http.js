ab35bb2b75983be7abcb5707956a7a2aec1c45b0
mohitacharya4 <mohitacharya4@gmail.com>
2023-06-25 14:43:55 +0530

    Performing CRUD operations using axios on data present in Firebase realtime database

---
+++ util/http.js
@@ -0,0 +1,41 @@
+import axios from "axios";
+
+const BACKEND_URL = "https://expense-tracker-d4213-default-rtdb.firebaseio.com";
+
+export async function storeExpense(expenseData) {
+  const response = await axios.post(
+    BACKEND_URL + "/expenses.json",
+    expenseData
+  );
+
+  // response from firebase (it stores id in name property)
+  const id = response.data.name;
+
+  return id;
+}
+
+export async function fetchExpenses() {
+  const response = await axios.get(BACKEND_URL + "/expenses.json");
+
+  const expenses = [];
+
+  for (const key in response.data) {
+    const expenseObj = {
+      id: key,
+      amount: response.data[key].amount,
+      date: new Date(response.data[key].date),
+      description: response.data[key].description,
+    };
+    expenses.push(expenseObj);
+  }
+
+  return expenses;
+}
+
+export async function updateExpense(id, expenseData) {
+    return await axios.put(BACKEND_URL + `/expenses/${id}.json`, expenseData);
+}
+
+export async function deleteExpense(id) {
+    return await axios.delete(BACKEND_URL + `/expenses/${id}.json`);
+}
